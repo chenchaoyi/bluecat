21b67e1448c532d2c075d931a4d4b75ce4708829
Chaoyi Chen <cchen0@walmart.com>
2014-03-31 15:41:45 -0700

    Add example and README

--- README.md
+++ README.md
@@ -2,3 +2,67 @@ bluecat
 =======
 
 A REST API testing framework built on node.js that makes testing API endpoints straightforward.
+
+Just define your APIs in a json file, Bluecat will create all the methods for you, plus it removes callbacks so tests that have a complex API call flow will be more clear.
+
+
+## Installation ##
+* Install [Node.js >= v0.10.25 and npm](http://nodejs.org/)
+* Install all node package dependencies:
+
+```bash
+$ npm install
+```
+
+## Write Test ##
+* Define your API in config/api.json
+* Test case example (using Mocha):
+
+First define your API in config/api.json:
+
+```
+{
+  "mobileapi": {
+    "typeahead": {
+      "schema": "http",
+      "method": ["GET"]
+      }
+  }
+}
+
+```
+
+Then in your test suite:
+
+```javascript
+var expect = require("chai").expect;
+var ServiceSync = require("bluecat").ServiceSync;
+var Api = require("bluecat").Api;
+
+var host = 'mobile.walmart.com';
+
+describe("typeahead service", function() {
+
+  before(function() {
+    t = new ServiceSync(Api("mobileapi"), host);
+  })
+
+  it("typeahead?term=xbo&cat=0&num=8", function(done) {
+    t.run(function() {
+      // send GET to typeahead?term=xbo&cat=0&num=8
+      var r = t.typeahead.GET({
+        term: 'xbox',
+        cat: 8,
+        num: 0
+      });
+
+      // verify response
+      expect(r.err).to.equal(null);
+      expect(r.data.statusCode).to.equal(200);
+      expect(r.data.body).to.have.ownProperty("specific");
+      done();
+    })
+  })
+})
+
+```
---
+++ examples/test/lapetus/cart.js
@@ -0,0 +1,60 @@
+// Sample Test suite showing bluecat framework
+
+var config = require("config");
+var expect = require("chai").expect;
+var ServiceSync = require("bluecat").ServiceSync;
+var Api = require("bluecat").Api;
+
+describe("Cart -> ", function() {
+  before(function() {
+    //lapetus = new ServiceSync(Api("lapetus", apiPath));
+    lapetus = new ServiceSync(Api("lapetus"), config.server.host);
+    lapetus.setProxy(config.server.proxy);
+  });
+
+  it("Create a new cart", function(done) {
+    lapetus.run(function() {
+      // create new cart
+      var r = lapetus.cart.POST({});
+      done();
+    });
+  });
+
+  it("Create a new cart with location", function(done) {
+    lapetus.run(function() {
+      // create new cart
+      var r = lapetus.cart.POST({
+        body: {
+          location: { postalCode:'94041' }
+        }
+      });
+      done();
+    });
+  });
+
+  it("Get cart with valid cart id", function(done) {
+    lapetus.run(function() {
+      // create new cart
+      var r = lapetus.cart.POST({});
+      expect(r.data.statusCode).to.equal(201);
+      var cart_id = r.data.body.cart.id;
+
+      // get cart
+      r = lapetus.cart["${cartid}"].GET({
+        params: {cartid: cart_id}
+      });
+      expect(r.data.statusCode).to.equal(200);
+      expect(r.data.body.items).to.eql([]);
+      expect(r.data.body.cart.id).to.eql(cart_id);
+      expect(r.data.body.cart.type).to.eql('ANONYMOUS');
+      expect(r.data.body.cart.itemCount).to.equal(0);
+      expect(r.data.body.cart.totals).to.eql({});
+      expect(r.data.body.cart.currencyCode).to.be.a('string');
+      expect(r.data.body.cart.customerId).to.be.a('string');
+      done();
+    });
+  });
+
+
+});
+
