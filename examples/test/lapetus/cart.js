489442525494bd3cfc1cb548045edca7e6476a6a
Chaoyi Chen <cchen0@walmart.com>
2015-05-20 15:27:57 -0700

    clean up sample

--- examples/test/lapetus/cart.js
+++ examples/test/lapetus/cart.js
@@ -7,35 +7,28 @@ var Api = require('bluecat').Api;
 
 describe('Cart -> ', function() {
   before(function() {
-    lapetus = new ServiceSync(Api('lapetus'), Config.server.host);
-    lapetus.setProxy(Config.server.proxy);
-  });
-
-  it('Create a new cart with location', function(done) {
-    lapetus.run(function() {
-      // create new cart
-      var r = lapetus.cart.POST({
-        body: {
-          location: { postalCode:'94041' }
-        }
-      });
-      done();
-    });
+    service = new ServiceSync(Api('api'), Config.server.host);
+    service.setProxy(Config.server.proxy);
   });
 
   it('Get cart with valid cart id', function(done) {
-    lapetus.run(function() {
+    service.run(function() {
       // create new cart
-      var r = lapetus.cart.POST({});
+      var r = service.cart.POST({
+        body: {
+          location: {
+            postalCode: '94041'
+          }
+        }
+      });
       expect(r.data.statusCode).to.equal(201);
       var cart_id = r.data.body.cart.id;
 
       // get cart
-      r = lapetus.cart['${cartid}'].GET({
+      r = service.cart['${cartid}'].GET({
         params: {cartid: cart_id}
       });
       expect(r.data.statusCode).to.equal(200);
-      expect(r.data.body.items).to.eql([]);
       expect(r.data.body.cart.id).to.eql(cart_id);
       expect(r.data.body.cart.type).to.eql('ANONYMOUS');
       expect(r.data.body.cart.itemCount).to.equal(0);
